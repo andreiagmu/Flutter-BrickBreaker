d3b87526368582914594cd4066ab28ae4c41da00
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-21 20:18:31 -0300

    Add collision detection (Ball, PlayArea)

--- lib/src/components/play_area.dart
+++ lib/src/components/play_area.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 import '../brick_breaker.dart';
 
 class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
-  PlayArea() : super(paint: Paint()..color = const Color(0xFFF2E8CF));
+  PlayArea() : super(
+    paint: Paint()..color = const Color(0xFFF2E8CF),
+    children: [RectangleHitbox()],
+  );
 
   @override
   FutureOr<void> onLoad() async {
