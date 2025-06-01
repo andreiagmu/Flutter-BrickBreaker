6ab159ce4ff83006cea1c0c2bfdb2b7603a5027c
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-06-01 09:38:37 -0300

    Add Brick component and brick-related logic

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -32,6 +32,7 @@ class BrickBreaker extends FlameGame
     world.add(PlayArea());
 
     world.add(Ball(
+      difficultyModifier: difficultyModifier,
       radius: ballRadius,
       position: size / 2,
       velocity: Vector2(
@@ -45,6 +46,18 @@ class BrickBreaker extends FlameGame
       cornerRadius: const Radius.circular(ballRadius / 2),
       position: Vector2(width / 2, height * 0.95),
     ));
+
+    await world.addAll([
+      for (var i = 0; i < brickColors.length; i++)
+        for (var j = 1; j <= 5; j++)
+          Brick(
+            position: Vector2(
+              (i + 0.5) * brickWidth + (i + 1) * brickGutter,
+              (j + 2.0) * brickHeight + j * brickGutter,
+            ),
+            color: brickColors[i],
+          ),
+    ]);
 
     debugMode = true;
   }
--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
+
+const brickColors = [
+  Color(0xFFF94144),
+  Color(0xFFF3722C),
+  Color(0xFFF8961E),
+  Color(0xFFF9844A),
+  Color(0xFFF9c74F),
+  Color(0xFF90BE6D),
+  Color(0xFF43AA8B),
+  Color(0xFF4D908E),
+  Color(0xFF277DA1),
+  Color(0xFF577590),
+];
+
 const gameWidth = 820.0;
 const gameHeight = 1600.0;
 
@@ -6,3 +21,10 @@ const ballRadius = gameWidth * 0.02;
 const batWidth = gameWidth * 0.2;
 const batHeight = ballRadius * 2;
 const batStep = gameWidth * 0.015;
+
+const brickGutter = gameWidth * 0.015;
+final brickWidth =
+    (gameWidth - (brickGutter * (brickColors.length + 1))) / brickColors.length;
+const brickHeight = gameHeight * 0.03;
+
+const difficultyModifier = 1.03;
